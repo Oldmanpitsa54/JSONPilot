bc18af43ff1c052aca015d8830478c47358f43c1
Oldmanpitsa54 <bublik12367@gmail.com>
2020-10-12 17:06:59 +0300

    Food Class realization

---
+++ Main/DL/Food.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DL
+{
+    public class CustomIgnoreAttribute : System.Attribute
+    {
+        public CustomIgnoreAttribute()
+        {
+
+        }
+    }
+
+        [Serializable]
+        public abstract class Food
+        {
+            [CustomIgnoreAttribute]
+            public string Name { get; set; }
+
+            [CustomIgnore]
+            public double Weight { get; set; }
+
+            public Food()
+            {
+
+            }
+
+            public Food(string name, double weight)
+            {
+                Name = name;
+                Weight = weight;
+            }
+        }
+
+
+    [Serializable]
+    public class Pitsa : Food
+    {
+        public uint Size { get; set; }
+
+        public Pitsa(string name, double weight, uint size)
+            : base(name, weight)
+        {
+            Size = Size;
+        }
+    }
+
+    [Serializable]
+    public class Burrito : Food
+    {
+        public bool IsSpicy { get; set; }
+
+        public Burrito(string name, double weight, bool isSpicy)
+            : base(name, weight)
+        {
+            IsSpicy = isSpicy;
+        }
+
+    }
+}
+
+    
+
