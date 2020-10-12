878c827f403026b832ff7d9a4bc582045b58a326
Oldmanpitsa54 <bublik12367@gmail.com>
2020-10-12 17:05:32 +0300

    Exception realization

---
+++ Main/DL/NoAvailablePropertiesException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DL
+{
+    public class NoAvailablePropertiesException : Exception
+    {
+        
+
+        public NoAvailablePropertiesException()
+            : base(String.Format("There are no properties for serialization"))
+        {
+
+        }
+    }
+}
